b1e0b5bf4da51e538dbb52ccfe01e7bc8e9ae601
sam-karis <samkaris75@gmail.com>
2017-01-28 01:26:19 +0300

    the cachematrix contain the function makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The program consist of two functions that is makeacheMatrix and CacheSolve
+## both functions combined compute and return the inverse of a matrix
+## the fuction also make a matrix object in cache so if you run the function again
+## using same matrix it retrieve the answer from the cache and if does not exist it compute the inverse
+## The function work under the assumption that the matrix x is a Square matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix create a special list containing functions to set and get a matrix 
+## also it set and get the iverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(), ...) {
+  m = NULL
+  set <- function(y){
+    m <<-  NULL
+    x <<- y
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
